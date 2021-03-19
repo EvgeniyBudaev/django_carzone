fa5b193bfe358d7a51899929219d23be08035c63
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-03-19 11:22:00 +0300

    Dashboard Functionality - Force User to Login

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
+
+from contacts.models import Contact
 
 
 # Create your views here.
@@ -54,8 +57,13 @@ def register(request):
     return render(request, 'accounts/register.html')
 
 
+@login_required(login_url = 'login')
 def dashboard(request):
-  return render(request, 'accounts/dashboard.html')
+  user_inquiry = Contact.objects.order_by('-created_date').filter(user_id=request.user.id)
+  data = {
+    'inquiries': user_inquiry,
+  }
+  return render(request, 'accounts/dashboard.html', data)
 
 
 def logout(request):
